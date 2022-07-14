e17dbc0606a9160e436bbb93484ac0dedbdfcc32
maryada <jainsubhamd@gmail.com>
2022-07-14 16:10:14 +0530

    Introduce money amount

--- money.go
+++ money.go
@@ -1,7 +1,6 @@
 package currency
 
 type Money struct {
-	rupee int64
 	paise int64
 }
 
@@ -9,16 +8,17 @@ func NewMoney(rupee, paise int64) Money {
 	if (rupee < 0 && paise > 0) || (rupee > 0 && paise < 0) {
 		panic("rupee and paise should have same sign")
 	}
-	if paise >= 100 || paise < 0 {
-		rupee += paise / 100
-		paise = paise % 100
-	}
+	paise = rupee*100 + paise
 
-	return Money{rupee, paise}
+	return Money{paise}
 }
 
 func (moneyOne Money) Add(moneyTwo Money) Money {
-	paise := moneyOne.rupee*100 + moneyTwo.rupee*100 + moneyOne.paise + moneyTwo.paise
+	paise := moneyOne.paise + moneyTwo.paise
 
 	return NewMoney(0, paise)
+}
+
+func (moneyOne Money) Amount() float64 {
+	return float64(moneyOne.paise) / 100
 }
