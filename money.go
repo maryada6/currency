20f7961738675a6a4f4235ccc7d023f60c2e0172
maryada <jainsubhamd@gmail.com>
2022-07-14 15:03:00 +0530

    Initialize money entity

--- money.go
+++ money.go
@@ -4,3 +4,12 @@ type Money struct {
 	rupee int64
 	paise int64
 }
+
+func NewMoney(rupee, paise int64) Money {
+	if paise >= 100 {
+		rupee += paise / 100
+		paise = paise % 100
+	}
+
+	return Money{rupee, paise}
+}
--- money_test.go
+++ money_test.go
@@ -1,1 +1,22 @@
 package currency
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewMoney(t *testing.T) {
+	t.Run("should be able to initialize money with rupee and paise", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			NewMoney(1, 0)
+		})
+	})
+
+	t.Run("new money should return money", func(t *testing.T) {
+		assert.IsType(t, Money{}, NewMoney(0, 0))
+	})
+
+	t.Run("should convert excess paise to rupee", func(t *testing.T) {
+		assert.Equal(t, NewMoney(10, 10), NewMoney(0, 1010))
+	})
+}
